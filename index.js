9fb3c7ea8d3116ac3e78bb607295009ff15c843b
hata <hiroki.ata@gmail.com>
2015-10-27 21:54:22 -0700

    Add index.js for nodejs

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var kintone = require('./js/aexlib-kintone.js');
+
+module.exports = {
+    App: kintone.App,
+    Query: kintone.Query,
+    Record: kintone.Record,
+    hookKintoneAPI: kintone.hookKintoneAPI
+};
+
+
